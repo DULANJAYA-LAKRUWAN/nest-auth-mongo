1bd6d8a17a09ba45f6367feb793ccb6df7a4103e
Malitha Supun <malithasupun2@gmail.com>
2024-12-09 13:58:46 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './users/user.module';
+import * as dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // Load environment variables
-    MongooseModule.forRoot(process.env.MONGO_URI), // MongoDB connection
+    MongooseModule.forRoot(process.env.mongo_uri), // MongoDB connection using correct variable
     AuthModule,
     UserModule,
   ],
